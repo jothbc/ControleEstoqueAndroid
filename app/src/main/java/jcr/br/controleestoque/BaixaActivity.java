11997381928396f2219cc0d97f67813150e22d5d
jothbc <jothbc@gmail.com>
2019-10-31 19:33:35 -0300

    *possivel ler código de barras com a camera

--- app/src/main/java/jcr/br/controleestoque/BaixaActivity.java
+++ app/src/main/java/jcr/br/controleestoque/BaixaActivity.java
@@ -2,17 +2,25 @@ package jcr.br.controleestoque;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.Gson;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import java.lang.reflect.Array;
+
 import jcr.br.controleestoque.Bean.Produto;
 
 
@@ -29,7 +37,6 @@ public class BaixaActivity extends AppCompatActivity {
         this.setTitle(getString(R.string.title_usuario) + " " + MainActivity.login_string.toUpperCase());
 
 
-
         //https://www.youtube.com/watch?v=125WPZHxU7E
 
         produto = new Produto();
@@ -43,6 +50,36 @@ public class BaixaActivity extends AppCompatActivity {
                 }
             }
         });
+
+        FloatingActionButton scan = findViewById(R.id.float_scan);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iniciarScan(view);
+            }
+        });
+    }
+
+    private void iniciarScan(View view) {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Camera Scan");
+        integrator.setCameraId(0);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if(result.getContents()!=null){
+                EditText codigo = findViewById(R.id.edtCodigo);
+                codigo.setText(result.getContents());
+                getQuantidade(findViewById(R.id.layout_content_baixa));
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     private void getQuantidade(View view) {
@@ -64,7 +101,7 @@ public class BaixaActivity extends AppCompatActivity {
             } else {
                 descricao.setText("");
                 quantidade.setText("");
-                Snackbar.make(view, "Falha", Snackbar.LENGTH_LONG).show();
+                Snackbar.make(view, (R.string.message_produto_null), Snackbar.LENGTH_LONG).show();
             }
         } catch (Exception e) {
             descricao.setText("");
@@ -114,16 +151,16 @@ public class BaixaActivity extends AppCompatActivity {
                 Snackbar.make(view, getString(R.string.message_concluido), Snackbar.LENGTH_LONG).show();
             } else {
                 AlertDialog dialog = new AlertDialog.Builder(this).create();
-                dialog.setTitle("Erro");
-                dialog.setMessage("Não foi possível realizar a operação.");
+                dialog.setTitle((R.string.title_erro));
+                dialog.setMessage(getString(R.string.message_erro_operacao));
                 dialog.show();
                 //Snackbar.make(view, resposta, Snackbar.LENGTH_LONG).show();
             }
         } catch (Exception e) {
             //Snackbar.make(view, e.getMessage(), Snackbar.LENGTH_LONG).show();
             AlertDialog dialog = new AlertDialog.Builder(this).create();
-            dialog.setTitle("Erro");
-            dialog.setMessage("Não foi possível realizar a operação.");
+            dialog.setTitle(R.string.title_erro);
+            dialog.setMessage(getString(R.string.message_erro_operacao));
             dialog.show();
         }
 
